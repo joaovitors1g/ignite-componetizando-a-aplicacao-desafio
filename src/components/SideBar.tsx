7968920a2cc292818f4b661818ba92de8a1f173c
joaovitors1g <joao_vitor9846@hotmail.com>
2022-05-29 21:59:14 -0300

    feat: separa content e nav em componentes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  handleSelectGenreId: (id: number) => void;
+  selectedGenreId: number;
+};
+
+export function SideBar({
+  genres,
+  handleSelectGenreId,
+  selectedGenreId,
+}: SideBarProps) {
+  function handleClickButton(id: number) {
+    handleSelectGenreId(id);
+  }
+
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
